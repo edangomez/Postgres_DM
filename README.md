024538912450ef2230fab6f013110d3c4217d7f0
Daniel gomez benavides <ed.gomez10@uniandes.edu.co>
2022-11-07 18:51:51 -0500

    updates

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Project: Data Modelling with Postgres
 
+In this project we practice data modelling with Postgres. As context, a music streaming startup called Sparkify wants  to create a Postgres database with tables designed to optimize queries on song play analysis, and bring you on the project. Hence, the two tasks are expected from a data engineer:
+
+- Create a database star schema
+- Write an ETL pipeline that transfers data from files in two local directories into these tables in Postgres using Python and SQL.
+
+This repository contains three ```.py``` files:
+- ```create_tables.py``` that create the necesary tables for the star schema
+- ```sql_queries.py``` is a file containing the SQL queries for the creation of the tables and the ETL process. This file is imported by the other to ```.py``` files in this repo.
+- ```etl.py``` which contains the main commands for the ETL pipeline. 
+
+## Datasets
+
+Two datasets are used for the ETL pipeline: logs of user activity and metadata of the songs in their app, both in JSON files.
+### Song dataset
+
+Each JSON file of this dataset contains metadata about a song and the artist of that song. The files are partitioned by the first three letters of each song's track ID. For example, here are file paths to two files in this dataset.
+
+```
+song_data/A/B/C/TRABCEI128F424C983.json
+song_data/A/A/B/TRAABJL12903CDCF1A.json
+```
+
+And below is an example of what a single song file, TRAABJL12903CDCF1A.json, looks like.
+
+````
+{"num_songs": 1, "artist_id": "ARJIE2Y1187B994AB7", "artist_latitude": null, "artist_longitude": null, "artist_location": "", "artist_name": "Line Renaud","song_id": "SOUPIRU12A6D4FA1E1", "title": "Der Kleine Dompfaff", "duration": 152.92036, "year": 0}
+````
+
+### Log dataset
+
+The second dataset consists of files in JSON of logs from the music streaming app. The log files in the dataset are partitioned by year and month. For example, here are filepaths to two files in this dataset.
+
+````
+log_data/2018/11/2018-11-12-events.json
+log_data/2018/11/2018-11-13-events.json
+````
+And below is an example of what the data in a log file, 2018-11-12-events.json, looks like.
+
+![image](https://video.udacity-data.com/topher/2019/February/5c6c15e9_log-data/log-data.png)
--- sql_queries.py
+++ sql_queries.py
@@ -8,46 +8,89 @@ time_table_drop = "DROP table time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""CREATE TABLE IF NOT EXTIS songplays (songplay_id int,\
-                        start_time int, user_id int, level int, song_id int, \
-                        artis_id int, session_id int, location varchar, \
+songplay_table_create = ("""CREATE TABLE IF NOT EXIST songplays (\
+                        songplay_id serial PRIMARY_KEY,\
+                        start_time int, \
+                        user_id int NOT NULL, \
+                        level int, \
+                        song_id int NOT NULL, \
+                        artis_id int NOT NULL, \
+                        session_id int NOT NULL, \
+                        location varchar, \
                         user_agent varchar);""")
 
-user_table_create = ("""CREATE TABLE IF NOT EXTIS user (user_id int, first_name varchar,\
-                        last_name varchar, gender varchar, level int);""")
+user_table_create = ("""CREATE TABLE IF NOT EXIST users (\
+                    user_id serial PRIMARY_KEY,\
+                    first_name varchar NOT NULL,\
+                    last_name varchar NOT NULL, \
+                    gender varchar, \
+                    level int);""")
 
 
-song_table_create = ("""CREATE TABLE IF NOT EXTIS song (song_id int, title varchar,\
-                    artist_id int, year int, duration );""")
+song_table_create = ("""CREATE TABLE IF NOT EXIST songs (\
+                    song_id serial PRIMARY KEY, \
+                    title varchar NOT NULL,\
+                    artist_id int NOT NULL, \
+                    year int, \
+                    duration timestamp NOT NULL);""")
 
-artist_table_create = ("""
-""")
+artist_table_create = ("""CREATE TABLE IF NOT EXIST artists (\
+                    artist_id serial PRIMARY KEY, \
+                    name varchar NOT NULL,\
+                    location varchar, \
+                    year int, \
+                    duration decimal NOT NULL, \
+                    latitude decimal, \
+                    longitude decimal);""")
 
-time_table_create = ("""
-""")
+time_table_create = ("""CREATE TABLE IF NT EXIST time (\
+                    start_time time_stamp PRIMARY KEY, \
+                    hour time_stamp NOT NULL,\
+                    day int NOT NULL, \
+                    week int NOT NULL, \
+                    month int NOT NULL, \
+                    year int NOT NULL, \
+                    weekday varchar);""")
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
-""")
+songplay_table_insert = ("""INSERT INTO songplays (songplay_id,\
+                                                    start_time,
+                                                    used_id,
+                                                    level,
+                                                    song_id,
+                                                    artist_id,
+                                                    session_id,
+                                                    location,
+                                                    user_agent)
+                                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                                ON CONFLICT (songplay_id)
+                                DO NOTHINGF;""")
 
-user_table_insert = ("""
-""")
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level)
+                        VALUES (%s, %s, %s, %s, %s)
+                        ON CONFLICT (user_id)
+                        DO NOTHING;""")
 
-song_table_insert = ("""
-""")
+song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration)\
+                        VALUES (%s, %s, %s, %s, %s)
+                        ON CONFLICT (song_id)
+                        DO NOTHING;""")
 
-artist_table_insert = ("""
-""")
+artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude)\
+                        VALUES (%s, %s, %s, %s, %s)
+                        ON CONFLICT (artist_id)
+                        DO NOTHING;""")
 
 
-time_table_insert = ("""
-""")
+time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday)\
+                        VALUES (%s, %s, %s, %s, %s, %s, %s)""")
 
 # FIND SONGS
 
-song_select = ("""
-""")
+song_select = ("""SELECT s.song_id, a.artist_id
+                FROM songs AS s JOIN artists AS a 
+                    ON s.artist_id = a.artis_id""")
 
 # QUERY LISTS
 
